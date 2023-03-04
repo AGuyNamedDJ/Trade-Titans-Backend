0ccb4aca724145fa1ede8347bf35f4baa3598be6
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-03-04 14:34:09 -0600

    revision

--- account/views.py
+++ account/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+# def say_hello(request):
+#     return HttpResponse("Hello World")
 
 
 def say_hello(request):
-    return HttpResponse("Hello World")
+    x = 1
+    y = 2
+    return render(request, 'hello.html')
